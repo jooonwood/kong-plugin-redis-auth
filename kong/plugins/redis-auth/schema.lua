272e64d19644498d914f5eae0cece78f7b69b0be
jooonwood <jooonwood@gmail.com>
2019-09-22 13:27:52 +0800

    fix

--- kong/plugins/redis-auth/schema.lua
+++ kong/plugins/redis-auth/schema.lua
@@ -2,7 +2,7 @@ local typedefs = require "kong.db.schema.typedefs"
 
 
 return {
-  name = "redisauth",
+  name = "redis-auth",
   fields = {
     { consumer = typedefs.no_consumer },
     { run_on = typedefs.run_on_first },
