1b9b7ab4a6f95ae3aadbe1458c3e5422621c9082
jooonwood <45617392+jooonwood@users.noreply.github.com>
2020-04-14 07:13:52 +0800

    Update 02-access_spec.lua

--- spec/redis-auth/02-access_spec.lua
+++ spec/redis-auth/02-access_spec.lua
@@ -87,6 +87,9 @@ for _, strategy in helpers.each_strategy() do
         local r = client:get("/public", {
           headers = {
             host = "test1.com"
+          },
+          query = {
+            apikey = "error"
           }
         })
         -- validate that the request succeeded, response status 200
