7c5586b160178abebc577b9371ad8353bbd76569
jooonwood <45617392+jooonwood@users.noreply.github.com>
2020-04-17 08:59:29 +0800

    Update handler.lua

--- kong/plugins/redis-auth/handler.lua
+++ kong/plugins/redis-auth/handler.lua
@@ -171,7 +171,7 @@ function RedisAuthHandler:access(conf)
   local ok, err = do_authentication(conf)
 
   if err then
-    if conf.anonymous then
+    if true then
       local request_path = kong.request.get_path()..'/'
       for i, v in ipairs(conf.anonymous_paths) do
         local match_path = v..'/'
@@ -182,7 +182,6 @@ function RedisAuthHandler:access(conf)
         end
       end
     end
-    set_consumer(cjson.decode(conf.anonymous_consumer), conf.consumer_keys)
     return kong.response.exit(err.status, { message = err.message }, err.headers)
   end
 end
