b2998223a1b0636ddecb3f1127d7507672ce9fdc
jooonwood <45617392+jooonwood@users.noreply.github.com>
2020-04-17 14:41:09 +0800

    Update 02-access_spec.lua

--- spec/redis-auth/02-access_spec.lua
+++ spec/redis-auth/02-access_spec.lua
@@ -97,17 +97,13 @@ for _, strategy in helpers.each_strategy() do
 
     describe("response", function()
       it("gets a 'anonymous' header", function()
-        local r = client:get("/anything", {
+        local res = client:get("/anything", {
           headers = {
             host = "test1.com"
           }
         })
-        -- validate that the request succeeded, response status 200
-        assert.response(r).has.status(200)
-        -- now check the response to have the header
-        local header_value = assert.request(r).has.header("X-Consumer-id")
-        -- validate the value of that header
-        assert.equal("this is on the response", header_value)
+        local body = cjson.decode(assert.res_status(200, res))
+        assert.equal('2000', body.headers["X-Consumer-id"])
       end)
     end)
 
