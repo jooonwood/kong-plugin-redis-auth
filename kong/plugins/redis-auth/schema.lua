9231ef809873f1e2a2d2840caba7e2fe83ecf975
jooonwood <45617392+jooonwood@users.noreply.github.com>
2020-04-20 06:55:28 +0800

    Update schema.lua

--- kong/plugins/redis-auth/schema.lua
+++ kong/plugins/redis-auth/schema.lua
@@ -17,7 +17,7 @@ return {
           }, },
           { hide_credentials = { type = "boolean", default = false }, },
           { anonymous = { type = "boolean", default = false }, },
-          { anonymous_consumer = { type = "table" }, },
+          { anonymous_consumer = { type = "map" }, },
           { anonymous_paths = { 
             type = "array", elements = { type = "string" }, default = { "public" } 
           }, },
