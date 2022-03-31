d05c0bcfe6bc3d9cc3fc9f59993c5aee0a240804
jooonwood <45617392+jooonwood@users.noreply.github.com>
2022-04-01 01:11:40 +0800

    Update handler.lua

--- kong/plugins/redis-auth/handler.lua
+++ kong/plugins/redis-auth/handler.lua
@@ -59,7 +59,10 @@ local function load_consumer(conf, key)
   if service and service.name then
     local service_exists = red:exists(conf.redis_key_prefix ..'services:'.. service.name)
     if service_exists then
-      local service_res = red:smismember(conf.redis_key_prefix ..'services:'.. service.name, 'public', uid)
+      red:init_pipeline()
+      red:sismember(conf.redis_key_prefix ..'services:'.. service.name, 'public')
+      red:sismember(conf.redis_key_prefix ..'services:'.. service.name, uid)
+      local service_res, service_err = red:commit_pipeline()
       local service_auth = 0
       for k, v in ipairs(service_res) do
         service_auth = service_auth + v
