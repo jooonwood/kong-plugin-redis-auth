5d50a24b6ad33ef631a42ab49054a61cb589bd6b
jooonwood <45617392+jooonwood@users.noreply.github.com>
2022-03-31 22:21:06 +0800

    Update handler.lua

--- kong/plugins/redis-auth/handler.lua
+++ kong/plugins/redis-auth/handler.lua
@@ -60,9 +60,9 @@ local function load_consumer(conf, key)
     local service_exists = red:exists(conf.redis_key_prefix ..'services:'.. service.name)
     if service_exists then
       local service_auth_everyone, service_auth_user = red:smismember(conf.redis_key_prefix ..'services:'.. service.name, 'public', uid)
-      if false == service_auth_everyone and false == service_auth_user then
-        --return nil, { status = 401, message = "access failed"..service.name..service_auth_user }
-        return nil, { status = 403, message = "access failed"..service.name..service_auth_user }
+      if false == service_auth_everyone then
+        return nil, { status = 401, message = "access failed"..service.name..service_auth_user }
+        --return nil, { status = 403, message = "access failed"..service.name..service_auth_user }
       end
     end
   end
